29c3bd9a6e1b493540832a3568d0423c1d84ff48
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-08-03 14:22:43 -0300

    add more dates

--- src/main/java/com/example/demo/controllers/TransactionController.java
+++ src/main/java/com/example/demo/controllers/TransactionController.java
@@ -5,10 +5,15 @@ import com.example.demo.models.Transaction;
 import com.example.demo.repositories.CardRepository;
 import com.example.demo.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -17,6 +22,7 @@ public class TransactionController {
 
   private final TransactionRepository transactionRepository;
   private final CardRepository cardRepository;
+  private final Set<Integer> usedTicketNumbers = new HashSet<>(); // Para almacenar los números de ticket utilizados
 
   @Autowired
   public TransactionController(TransactionRepository transactionRepository, CardRepository cardRepository) {
@@ -52,4 +58,57 @@ public class TransactionController {
   public List<Transaction> getAllTransactions() {
     return transactionRepository.findAll();
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
+    Optional<Transaction> transaction = transactionRepository.findById(id);
+
+    if (transaction.isPresent()) {
+      return ResponseEntity.ok(transaction.get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping("/withdraw")
+  public Transaction simulateWithdrawal(@RequestBody Transaction transaction) {
+    Card senderCard = cardRepository.findById(transaction.getSenderCardId()).orElse(null);
+
+    if (senderCard == null) {
+      throw new IllegalArgumentException("Tarjeta de envío no encontrada");
+    }
+
+    double amount = transaction.getAmount();
+    double senderBalance = senderCard.getBalance();
+
+    if (senderBalance < amount) {
+      throw new IllegalArgumentException("Saldo insuficiente en la tarjeta de envío");
+    }
+
+    // Generar un número de ticket aleatorio no repetido
+    int ticketNumber = generateUniqueTicketNumber();
+
+    senderCard.setBalance(senderBalance - amount);
+    cardRepository.save(senderCard);
+
+    transaction.setTransactionDate(new Date());
+    transaction.setTicketNumber(ticketNumber); // Establecer el número de ticket generado
+
+    return transactionRepository.save(transaction);
+  }
+
+  // Generar un número de ticket aleatorio no repetido
+  private int generateUniqueTicketNumber() {
+    Random random = new Random();
+    int ticketNumber;
+
+    // Generar números aleatorios hasta encontrar uno no utilizado previamente
+    do {
+      ticketNumber = random.nextInt(100000); // Puedes ajustar el rango según tus necesidades
+    } while (usedTicketNumbers.contains(ticketNumber));
+
+    usedTicketNumbers.add(ticketNumber);
+    return ticketNumber;
+  }
+
 }
--- src/main/java/com/example/demo/models/Transaction.java
+++ src/main/java/com/example/demo/models/Transaction.java
@@ -14,6 +14,7 @@ public class Transaction {
     private String receiverCardId;
     private double amount;
     private Date transactionDate;
+    private int ticketNumber;
 
     public String getId() {
         return id;
@@ -53,5 +54,13 @@ public class Transaction {
 
     public void setTransactionDate(Date transactionDate) {
         this.transactionDate = transactionDate;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
     }
 }
